67c66864ad3af3fe5324bb0ebfff9c5eab469904
blossomkaler <54261097+blossomkaler@users.noreply.github.com>
2023-04-12 17:21:01 +0530

    event delegation

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ addTodo.addEventListener('keyup',function(e){
     }
 });
 
-function populateList(tasks, taskList) {   //tasks is an array of objects
+function populateList(tasks, taskList) {   //tasks will be an array of objects (todos)
     taskList.innerHTML = tasks.map((task, i) => {
         return `
         <li>
@@ -34,20 +34,29 @@ function populateList(tasks, taskList) {   //tasks is an array of objects
 populateList(todos, todoList);    //ensures thats TASKS from local storage are retrieved first
 
 
-/*````````````````````Handling check on page refresh``````````````````````
-itemsList.addEventListener('click', toggleDone);
+/*````````````````````Handling check on page refresh``````````````````````*/
+
+todoList.addEventListener('click', toggleDone);   
 
 function toggleDone(e) {
-    if (!e.target.matches('input')) return; // skip this unless it's an input
+    if (!e.target.matches('input')) return;    // skip this unless it's input
+
     const el = e.target;
     const index = el.dataset.index;
-    items[index].done = !items[index].done;
-    localStorage.setItem('items', JSON.stringify(items));
-    populateList(items, itemsList);
-  }
+    let checkedElement =document.querySelector("label[for='" + el.id + "']");
+
+    todos[index].done = !todos[index].done;
+/* 
+    if(todos[index].done) checkedElement.classList.add('checked-item');
+    else checkedElement.classList.remove('checked-item'); */
+    
+        
+    localStorage.setItem('TASKS', JSON.stringify(todos));
+    populateList(todos, todoList);
   
-  
-``````````````````````````CHANGE THEME````````````````````````````````*/
+}
+
+/*``````````````````````````CHANGE THEME````````````````````````````````*/
 const sunIcon = document.querySelector('.sun-icon');
 const moonIcon = document.querySelector('.moon-icon');
 const bgImg = document.querySelector('.bg');
