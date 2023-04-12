a57a16c75e55312475d7421d113b838c82f82f9b
blossomkaler <54261097+blossomkaler@users.noreply.github.com>
2023-04-12 13:06:15 +0530

    local storage

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*``````````````````````````ADDING TODOs````````````````````````````````*/
 const addTodo = document.querySelector('.add-todo');
 const todoList = document.querySelector('.list-preview');
-const todos =[];
+const todos = JSON.parse(localStorage.getItem('TASKS')) || [];   // check for TASKS in local storage or start with empty array or no todos
 
 
 addTodo.addEventListener('keyup',function(e){
@@ -15,6 +15,7 @@ addTodo.addEventListener('keyup',function(e){
 
           todos.push(todo);
           populateList(todos, todoList);
+          localStorage.setItem('TASKS', JSON.stringify(todos));  // adding todos array to local storage under title-TASKS
           document.querySelector('[name = todo]').value = '';
     }
 });
@@ -30,24 +31,23 @@ function populateList(tasks, taskList) {   //tasks is an array of objects
     }).join('');
 }
 
-/*````````````````````LINE THROUGH CHECKED TODOs``````````````````````*/
+populateList(todos, todoList);    //ensures thats TASKS from local storage are retrieved first
 
 
+/*````````````````````Handling check on page refresh``````````````````````
+itemsList.addEventListener('click', toggleDone);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*``````````````````````````CHANGE THEME````````````````````````````````*/
+function toggleDone(e) {
+    if (!e.target.matches('input')) return; // skip this unless it's an input
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+  }
+  
+  
+``````````````````````````CHANGE THEME````````````````````````````````*/
 const sunIcon = document.querySelector('.sun-icon');
 const moonIcon = document.querySelector('.moon-icon');
 const bgImg = document.querySelector('.bg');
